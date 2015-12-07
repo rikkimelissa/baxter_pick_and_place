b19bf5d05648cb6274600ec215790c684d201521
Matthew Cruz <mattcruz10@gmail.com>
2015-12-07 15:57:24 -0600

    adding newest baxter_vis_node working and copy baxter_vis_node1.py

--- src/scripts/baxter_vis_node.py
+++ src/scripts/baxter_vis_node.py
@@ -1,156 +1,30 @@
 #!/usr/bin/env python  
 import roslib
 import rospy
-import math
+from math import pi, cos, sin
 import tf
 import numpy as np
-import cv2
-import cv2.cv as cv1
-from cv_bridge import CvBridge, CvBridgeError
-
-#http://wiki.ros.org/tf/Overview/Transformations
-#http://wiki.ros.org/tf/Overview/Using%20Published%20Transforms
-#to create an offset we can use the following(sec 8.1, 8.2):
+#import cv2
+#import cv2.cv as cv1
+#from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image 
-from ar_track_alvar_msgs.msg import AlvarMarker
-#from sensor_msgs.msg import JointState 
 from geometry_msgs.msg import PoseStamped, Pose, Point, Quaternion, QuaternionStamped,Vector3Stamped
-from std_msgs.msg import Header
-#from quat import quat_to_so3, so3_to_quat
+from std_msgs.msg import Header, Int16
+from quat import quat_to_so3, so3_to_quat
+from functions import RpToTrans, TransToRp
 
-
+    
 def xdisplay_pub(data,count):
-    print "entered"
-    if count%20 ==0:
-        #img_pub = rospy.Publisher('/robot/xdisplay', Image, latch=True, queue_size=100)
-        #img_pub.publish(data)
-        print "image publishing"
-
-def starting_arm_position():
-    #enter something here 
-    x =1
-    
-def circle_find(img_msg,count):
-
-    bridge = CvBridge()
-    if count%30 == 0:
-
-        try:
-           frame1 = bridge.imgmsg_to_cv2(img_msg, "bgr8")
-           #cv2.VideoCapture()
-           cv_image = frame1
-           cv2.imwrite(cv_image.png,cv_image)
-        except CvBridgeError, e:
-            print("==[CAMERA MANAGER]==", e)
-        #red = np.uint([[[255,0,0]]])
-
-        #hsv_red = cv2.cvtColor(red,cv2.COLOR_BGR2HSV)
-        #print hsv_red
-        print cv_image.shape            
-        img = cv2.medianBlur(cv_image,5)
-
-        
-        gray_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-        
-        circles = cv2.HoughCircles(gray_img,cv1.CV_HOUGH_GRADIENT,1,20,
-                                    param1=50,param2=30,minRadius=1,maxRadius=20)
-
-        #circles = np.uint16(np.around(circles))
-        '''
-        if len(circles)>0:
-            for i in circles[0,:]:
-                # draw the outer circle
-                cv2.circle(gray_img,(i[0],i[1]),i[2],(0,255,0),2)
-                # draw the center of the circle
-                cv2.circle(gray_img,(i[0],i[1]),2,(0,0,255),3)
-
-        cv2.imshow('detected circles',gray_img)
-
-        #convert open cv image to ROS image
-        '''
-        im_m = bridge.cv2_to_imgmsg(gray_img, "mono8")
-        pub_img = rospy.Publisher('image_topic',Image, queue_size = 10)
-        pub_img.publish(im_m)
-
-        '''
-        cv2.waitKey(15)
-
-        cv2.destroyAllWindows()
-        '''
-    
-    
-def pub_tag_pose(tag_transform,WCtransform):
-    # you enter here posCB and quatCB which is pose data in quaternions
-    #listener = tf.TransformListener()
-    #marker_id = 'ar_marker_%d'%tag_id
-    #print "pub_tag_pose: markerid", marker_id
-    #marker_found = listener.frameExists('ar_marker_%d'%tag_id)
-    #print marker_found
-    print tag_transform
-    (posWB, quatWB) = tag_transform
-
-    bpos = Pose()
-
-    bpos.position.x = posWB[0]
-    bpos.position.y = posWB[1]
-    bpos.position.z = posWB[2]
-    bpos.orientation.x = quatWB[1]
-    bpos.orientation.y = quatWB[2]
-    bpos.orientation.z = quatWB[3]
-    bpos.orientation.w = quatWB[0]
-
-    tag_pub = rospy.Publisher('block_position', Pose, queue_size = 10)
-    tag_pub.publish(bpos)
-    '''
-    quatWC = [quatWC[1:4], quatWC[0]]
-    quatCB = [quatCB[1:4], quatCB[0]]
-
-    rotWC = quat_to_so3(quatWC)
-    rotCB = quat_to_so3(quatCB)
-    gWC = RpToTrans(rotWC,posWC)
-    gCB = RpToTrans(rotCB,posCB)
-    gWB = gWC.dot(gCB)
-    rotWB, posWB = TransToRp(gWB)
-    quatWB = so3_to_quat(rotWB)
-    '''
+    #print "entered"
+    img_pub = rospy.Publisher('/robot/xdisplay', Image, latch=True, queue_size=100)
+    img_pub.publish(data)
+    rospy.sleep(0.25)
 
 
+def set_state_callback(data):
+    state = data.data
 
-def multi_marker(listener,tag_found):
-        
-    #Creating a list for multiple markers and their locations
-    trans = []
-    rot = []
-    dist = []
-    id_list = []
-    tag_pose = Pose()
-    while tag_found == False:  
-        for n in range(1,7):
-            marker_id = 'ar_marker_%d'%n
-            marker_found = listener.frameExists(marker_id)
-            if marker_found == True:
-                #print "looking at tag:",n
-                (trans_new,rot_new) = listener.lookupTransform('right_hand_camera', marker_id, rospy.Time(0))
-                trans.append(trans_new)
-                rot.append(rot_new)
-                dist.append(np.linalg.norm(trans_new))
-                id_list.append(n)
-                print marker_id, ":", trans_new
-        if len(dist)>0:     
-            tag_index = np.argmin(dist)
-            print "tag index is", tag_index
-            tag_selected = id_list[tag_index]
-            tag_transform =  (trans[tag_index],rot[tag_index])
-            tag_found = True
-            listener.waitForTransform('base', 'right_hand_camera', rospy.Time(0), rospy.Duration(.25))
-            (transWC,rotWC) = listener.lookupTransform('base', 'right_hand_camera', rospy.Time(0))
-            WCtransform = (transWC,rotWC)
-            print "Distance is:", dist[np.argmin(dist)]  
-        #could add a statement in here to see if block is too close to neighbors
-          
-    return tag_found, tag_transform, WCtransform
-       
-    
+
 def main_func():
     #img = Image()
     rospy.init_node('tag_select', anonymous=False)
@@ -162,35 +36,118 @@ def main_func():
     rot = []
     dist = []
     id_list = []
+    tag_pose = Pose()
     tag_found = False
-    state_machine = 1
+    state = 1
+    rtime = rospy.Time(0)
+    rospy.Subscriber('state',Int16,set_state_callback)
+    # while listener.frameExists('base') == False:
+
+    #     try:
+    #         (transWC,rotWC) = listener.lookupTransform('base', 'right_hand_camera', rospy.Time(0))
+    #     except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+    #         continue
+    rospy.sleep(1)
+
+
     while not rospy.is_shutdown():
+        #if count%10==0:
+        rospy.Subscriber("/cameras/right_hand_camera/image", Image,xdisplay_pub,count)
 
-        #if count%5 == 0
-        #    rospy.Subscriber("/cameras/right_hand_camera/image", Image, get_tag_pose)
+        #tag_found, tag_transform, WCtransform = multi_marker(listener,tag_found)
+        while tag_found == False:  
+            #posCB, quatCB, posWC, quatWC = multi_marker(listener)
+            for n in range(1,7):
+                marker_id = 'ar_marker_%d'%n
+                marker_found = listener.frameExists(marker_id)
+                if marker_found == True:
+                    #print "looking at tag:",n
+                    #(transWC,rotWC) = listener.lookupTransform('base', 'right_hand_camera', rtime)
+                    posCB,quatCB = listener.lookupTransform('right_hand_camera', marker_id, rtime)
+                    print "posCB:", posCB
+                    print "quatCB", quatCB
+                    trans.append(posCB)
+                    rot.append(quatCB)
+                    dist.append(np.linalg.norm(posCB))
+                    id_list.append(n)
+                    print marker_id, ":", posCB
+            if len(dist)>0:     
+                tag_index = np.argmin(dist)
+                print "tag index is", tag_index
+                tag_selected = id_list[tag_index]
+                tag_transform =  (trans[tag_index],rot[tag_index])
+                tag_found = True
+                #listener.waitForTransform('base', 'right_hand_camera', rospy.Time(0), rospy.Duration(.5))
+                
+                posCB, quatCB = tag_transform
+                posWC,quatWC = listener.lookupTransform('base', 'right_hand_camera', rospy.Time(0))
 
-        #This puts the image onto Baxter's head every 2 seconds by calling the xdisplay_pub topic
-        #rospy.Subscriber("/usb_cam/image_raw", Image,xdisplay_pub,count)
-        #rospy.Subscriber("/cameras/right_hand_camera/image", Image,xdisplay_pub,count)
+                print "posCB after if:", posCB
+                print "quatCB acter if:", quatCB
 
-        ###This piece is for finding circles###
-        ###not finished yet, i only want to save one picture every second or two and use that###
-        #rospy.Subscriber("/usb_cam/image_raw", Image, circle_find,count)
-        #rospy.Subscriber("/usb_cam/image_raw", Image, circle_find,count)
+                print "posWC", posWC
+                print "quatWC", quatWC
 
-        ###This piece finds the closest marker when multiple unique markers are used###
-        #rospy.Subscriber("/usb_cam/image_raw", Image, multi_marker,listener,tag_found)
-        tag_found, tag_transform, WCtransform = multi_marker(listener,tag_found)
-        #pub_tag_pose(listener,tag_selected)        
 
+
+                posCB = np.array([posCB[0], posCB[1], posCB[2]])
+                posWC = np.array([posWC[0], posWC[1], posWC[2]])
+                quatCB = np.array([quatCB[3],quatCB[0],quatCB[1],quatCB[2]])
+                quatWC = np.array([quatWC[3],quatWC[0],quatWC[1],quatWC[2]])
+                
+
+                # print tag_transform
+         
+                print "Distance is:", dist[np.argmin(dist)]  
 
         count += 1
-        #print count
-        if tag_found == True:
-            #print "tag selected", tag_selected
-            pub_tag_pose(tag_transform, WCtransform)
-            rospy.sleep(2)
-            break
+
+
+
+        #if tag_found == True:
+        #pub_tag_pose(tag_transform, WCtransform)
+        #print tag_transform
+       
+        rotWC = quat_to_so3(quatWC)
+        rotCB = quat_to_so3(quatCB)
+        gWC = RpToTrans(rotWC,posWC)
+        print "gWC is: ", gWC
+        gCB = RpToTrans(rotCB,posCB)
+        gWB = gWC.dot(gCB)
+        beta = pi
+        gpick = gWB.dot(np.array([[cos(beta),0, -sin(beta),0],[0,1,0,0],[sin(beta),0, cos(beta),0],[0,0,0,1]]))
+        print "gWB is: ", gWB
+        print "gCB is: ", gCB
+        rotWB, posWB = TransToRp(gpick)
+        quatWB = so3_to_quat(rotWB)
+
+        print "quatWB", quatWB
+        print "PosWB:", posWB
+
+        bpos = Pose()
+        bpos.position.x = posWB[0]
+        bpos.position.y = posWB[1]
+        bpos.position.z = posWB[2]
+        bpos.orientation.x = quatWB[1]
+        bpos.orientation.y = quatWB[2]
+        bpos.orientation.z = quatWB[3]
+        bpos.orientation.w = quatWB[0]
+        tag_found = False
+
+        state_pub = rospy.Publisher('state', Int16, queue_size=10)
+        tag_pub = rospy.Publisher('block_position', Pose, queue_size = 10)
+
+        for i in range(10):
+            # state_pub = rospy.Publisher('state', Int16, queue_size=10)
+            state_pub.publish(2)
+
+            # tag_pub = rospy.Publisher('block_position', Pose, queue_size = 10)
+            tag_pub.publish(bpos)
+
+            print "tag selected", tag_selected  
+        #rospy.sleep(1)
+            #break
+        print state
         rate.sleep()
 
 
--- src/scripts/baxter_vis_node1.py
+++ src/scripts/baxter_vis_node1.py
@@ -1,80 +1,30 @@
 #!/usr/bin/env python  
 import roslib
 import rospy
-import math
+from math import pi, cos, sin
 import tf
 import numpy as np
-import cv2
-import cv2.cv as cv1
-from cv_bridge import CvBridge, CvBridgeError
-
-#http://wiki.ros.org/tf/Overview/Transformations
-#http://wiki.ros.org/tf/Overview/Using%20Published%20Transforms
-#to create an offset we can use the following(sec 8.1, 8.2):
+#import cv2
+#import cv2.cv as cv1
+#from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image 
-#from ar_track_alvar_msgs.msg import AlvarMarker
-#from sensor_msgs.msg import JointState 
 from geometry_msgs.msg import PoseStamped, Pose, Point, Quaternion, QuaternionStamped,Vector3Stamped
-from std_msgs.msg import Header
-#from quat import quat_to_so3, so3_to_quat
+from std_msgs.msg import Header, Int16
+from quat import quat_to_so3, so3_to_quat
+from functions import RpToTrans, TransToRp
 
     
-def pub_tag_pose(tag_transform,WCtransform):
-
-    print tag_transform
-    (posWB, quatWB) = tag_transform
-
-    bpos = Pose()
-
-    bpos.position.x = posWB[0]
-    bpos.position.y = posWB[1]
-    bpos.position.z = posWB[2]
-    bpos.orientation.x = quatWB[1]
-    bpos.orientation.y = quatWB[2]
-    bpos.orientation.z = quatWB[3]
-    bpos.orientation.w = quatWB[0]
-
-    tag_pub = rospy.Publisher('block_position', Pose, queue_size = 10)
-    tag_pub.publish(bpos)
+def xdisplay_pub(data,count):
+    #print "entered"
+    img_pub = rospy.Publisher('/robot/xdisplay', Image, latch=True, queue_size=100)
+    img_pub.publish(data)
+    rospy.sleep(0.25)
 
 
+def set_state_callback(data):
+    state = data.data
 
 
-def multi_marker(listener,tag_found):
-        
-    #Creating a list for multiple markers and their locations
-    trans = []
-    rot = []
-    dist = []
-    id_list = []
-    tag_pose = Pose()
-    while tag_found == False:  
-        for n in range(1,7):
-            marker_id = 'ar_marker_%d'%n
-            marker_found = listener.frameExists(marker_id)
-            if marker_found == True:
-                #print "looking at tag:",n
-                (trans_new,rot_new) = listener.lookupTransform('right_hand_camera', marker_id, rospy.Time(0))
-                trans.append(trans_new)
-                rot.append(rot_new)
-                dist.append(np.linalg.norm(trans_new))
-                id_list.append(n)
-                print marker_id, ":", trans_new
-        if len(dist)>0:     
-            tag_index = np.argmin(dist)
-            print "tag index is", tag_index
-            tag_selected = id_list[tag_index]
-            tag_transform =  (trans[tag_index],rot[tag_index])
-            tag_found = True
-            listener.waitForTransform('base', 'right_hand_camera', rospy.Time(0), rospy.Duration(.25))
-            (transWC,rotWC) = listener.lookupTransform('base', 'right_hand_camera', rospy.Time(0))
-            WCtransform = (transWC,rotWC)
-            print "Distance is:", dist[np.argmin(dist)]  
-        #could add a statement in here to see if block is too close to neighbors
-          
-    return tag_found, tag_transform, WCtransform
-       
-    
 def main_func():
     #img = Image()
     rospy.init_node('tag_select', anonymous=False)
@@ -88,32 +38,39 @@ def main_func():
     id_list = []
     tag_pose = Pose()
     tag_found = False
-    state_machine = 1
+    state = 1
     rtime = rospy.Time(0)
+    rospy.Subscriber('state',Int16,set_state_callback)
     # while listener.frameExists('base') == False:
 
     #     try:
     #         (transWC,rotWC) = listener.lookupTransform('base', 'right_hand_camera', rospy.Time(0))
     #     except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
     #         continue
-    rospy.sleep(2)
+    rospy.sleep(1)
+
+
     while not rospy.is_shutdown():
+        #if count%10==0:
+        rospy.Subscriber("/cameras/right_hand_camera/image", Image,xdisplay_pub,count)
 
         #tag_found, tag_transform, WCtransform = multi_marker(listener,tag_found)
         while tag_found == False:  
-            
+            #posCB, quatCB, posWC, quatWC = multi_marker(listener)
             for n in range(1,7):
                 marker_id = 'ar_marker_%d'%n
                 marker_found = listener.frameExists(marker_id)
                 if marker_found == True:
                     #print "looking at tag:",n
                     #(transWC,rotWC) = listener.lookupTransform('base', 'right_hand_camera', rtime)
-                    (trans_new,rot_new) = listener.lookupTransform('right_hand_camera', marker_id, rtime)
-                    trans.append(trans_new)
-                    rot.append(rot_new)
-                    dist.append(np.linalg.norm(trans_new))
+                    posCB,quatCB = listener.lookupTransform('right_hand_camera', marker_id, rtime)
+                    print "posCB:", posCB
+                    print "quatCB", quatCB
+                    trans.append(posCB)
+                    rot.append(quatCB)
+                    dist.append(np.linalg.norm(posCB))
                     id_list.append(n)
-                    print marker_id, ":", trans_new
+                    print marker_id, ":", posCB
             if len(dist)>0:     
                 tag_index = np.argmin(dist)
                 print "tag index is", tag_index
@@ -121,31 +78,76 @@ def main_func():
                 tag_transform =  (trans[tag_index],rot[tag_index])
                 tag_found = True
                 #listener.waitForTransform('base', 'right_hand_camera', rospy.Time(0), rospy.Duration(.5))
-                (transWC,rotWC) = listener.lookupTransform('base', 'right_hand_camera', rospy.Time(0))
-                WCtransform = (transWC,rotWC)
+                
+                posCB, quatCB = tag_transform
+                posWC,quatWC = listener.lookupTransform('base', 'right_hand_camera', rospy.Time(0))
+
+                print "posCB after if:", posCB
+                print "quatCB acter if:", quatCB
+
+                print "posWC", posWC
+                print "quatWC", quatWC
+
+
+
+                posCB = np.array([posCB[0], posCB[1], posCB[2]])
+                posWC = np.array([posWC[0], posWC[1], posWC[2]])
+                quatCB = np.array([quatCB[3],quatCB[0],quatCB[1],quatCB[2]])
+                quatWC = np.array([quatWC[3],quatWC[0],quatWC[1],quatWC[2]])
+                
+
+                # print tag_transform
+         
                 print "Distance is:", dist[np.argmin(dist)]  
+
         count += 1
 
 
 
-        if tag_found == True:
-            #pub_tag_pose(tag_transform, WCtransform)
-            print tag_transform
-            (posWB, quatWB) = tag_transform
+        #if tag_found == True:
+        #pub_tag_pose(tag_transform, WCtransform)
+        #print tag_transform
+       
+        rotWC = quat_to_so3(quatWC)
+        rotCB = quat_to_so3(quatCB)
+        gWC = RpToTrans(rotWC,posWC)
+        print "gWC is: ", gWC
+        gCB = RpToTrans(rotCB,posCB)
+        gWB = gWC.dot(gCB)
+        beta = pi
+        gpick = gWB.dot(np.array([[cos(beta),0, -sin(beta),0],[0,1,0,0],[sin(beta),0, cos(beta),0],[0,0,0,1]]))
+        print "gWB is: ", gWB
+        print "gCB is: ", gCB
+        rotWB, posWB = TransToRp(gpick)
+        quatWB = so3_to_quat(rotWB)
 
-            bpos = Pose()
-            bpos.position.x = posWB[0]
-            bpos.position.y = posWB[1]
-            bpos.position.z = posWB[2]
-            bpos.orientation.x = quatWB[1]
-            bpos.orientation.y = quatWB[2]
-            bpos.orientation.z = quatWB[3]
-            bpos.orientation.w = quatWB[0]
+        print "quatWB", quatWB
+        print "PosWB:", posWB
 
-            tag_pub = rospy.Publisher('block_position', Pose, queue_size = 10)
+        bpos = Pose()
+        bpos.position.x = posWB[0]
+        bpos.position.y = posWB[1]
+        bpos.position.z = posWB[2]
+        bpos.orientation.x = quatWB[1]
+        bpos.orientation.y = quatWB[2]
+        bpos.orientation.z = quatWB[3]
+        bpos.orientation.w = quatWB[0]
+        tag_found = False
+
+        state_pub = rospy.Publisher('state', Int16, queue_size=10)
+        tag_pub = rospy.Publisher('block_position', Pose, queue_size = 10)
+
+        for i in range(10):
+            # state_pub = rospy.Publisher('state', Int16, queue_size=10)
+            state_pub.publish(2)
+
+            # tag_pub = rospy.Publisher('block_position', Pose, queue_size = 10)
             tag_pub.publish(bpos)
-            rospy.sleep(2)
-            break
+
+            print "tag selected", tag_selected  
+        #rospy.sleep(1)
+            #break
+        print state
         rate.sleep()
 
 
