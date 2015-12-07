755d2d3147f62025c9ef4082a63bf887dea68f20
Matthew Cruz <mattcruz10@gmail.com>
2015-12-06 18:04:21 -0600

    added function so that the function: multi_marker is added to choose closest block, needs to be changed to only run once

--- src/scripts/baxter_vis_node.py
+++ src/scripts/baxter_vis_node.py
@@ -3,122 +3,131 @@ import roslib
 import rospy
 import math
 import tf
+import numpy as np
 #http://wiki.ros.org/tf/Overview/Transformations
 #http://wiki.ros.org/tf/Overview/Using%20Published%20Transforms
 #to create an offset we can use the following(sec 8.1, 8.2):
 from sensor_msgs.msg import Image 
 from ar_track_alvar_msgs.msg import AlvarMarker
 #from sensor_msgs.msg import JointState 
-from geometry_msgs.msg import PoseStamped, Pose, Point, Quaternion
+from geometry_msgs.msg import PoseStamped, Pose, Point, Quaternion, QuaternionStamped,Vector3Stamped
 from std_msgs.msg import Header
 
 
-
 def xdisplay_pub(data):
-    pub = rospy.Publisher('/robot/xdisplay', Image, latch=True, queue_size=100)
+    img_pub = rospy.Publisher('/robot/xdisplay', Image, latch=True, queue_size=100)
     #pub = rospy.Publisher('/robot_image', Image, latch=True, queue_size=10)
-    pub.publish(data)
+    img_pub.publish(data)
     
 def select_tag(data):
-    pub = rospy.Publisher('new_pose', Pose, latch=True, queue_size=10)
+    tag_pub = rospy.Publisher('new_pose', Pose, latch=True, queue_size=10)
     in_pose = Pose()
-    pub.publish(data)
+    tag_pub.publish(data)
 
     
 def get_tag_pose():
 
-    #subrate = rospy.Rate(1) # 10hz
     listener = tf.TransformListener()
-    #initialize lists
+    trans1 = 0
+    
+    print listener.frameExists('ar_marker_4')
+    print listener.frameExists('head_camera')
+    if listener.frameExists('ar_marker_4') == True and listener.frameExists('head_camera') ==True:
+        (trans1,rot1) = listener.lookupTransform('ar_marker_4', 'ar_marker_5', rospy.Time(0))
+        print trans1
+    
+    #call select tag function
+
+def multi_marker(trans,rot,dist,id_list,listener):
+        
+    #Creating a list for multiple markers and their locations
     trans = []
     rot = []
-
-    print "made it"
-    #using time http://wiki.ros.org/rospy/Overview/Time
-    now_time =rospy.Time.now()
-    #create a time of 10 seconds = time_10 = rospy.Time(10)
-    
-    dt = 0
-    try:
-        #(trans[0],rot[0]) = listener.lookupTransform('right_hand_camera', '/ar_marker_1', rospy.Time(0))
-        (trans[0],rot[0]) = listener.lookupTransform('/head_camera', '/ar_marker_1', rospy.Time(0))
-    except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-        continue
-    '''
-    try:
-        (trans[1],rot[1]) = listener.lookupTransform('right_hand_camera', '/ar_marker_2', rospy.Time(0))
-    except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-        continue
-    try:
-        (trans[2],rot[2]) = listener.lookupTransform('right_hand_camera', '/ar_marker_3', rospy.Time(0))
-    except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-        continue
-    try:
-        (trans[3],rot[3]) = listener.lookupTransform('right_hand_camera', '/ar_marker_4', rospy.Time(0))
-    except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-        continue
-    try:
-        (trans[4],rot[4]) = listener.lookupTransform('right_hand_camera', '/ar_marker_5', rospy.Time(0))
-    except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-        continue
-    try:
-        (trans[5],rot[5]) = listener.lookupTransform('right_hand_camera', '/ar_marker_6', rospy.Time(0))
-    except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-        continue
-    '''
+    dist = []
+    id_list = []
+    print len(dist)
+    while len(dist)<1:  
+        for n in range(1,7):
+            marker_id = 'ar_marker_%d'%n
+            marker_found = listener.frameExists(marker_id)
+            if marker_found == True:
+                print "looking at tag:",n
+                (trans_new,rot_new) = listener.lookupTransform('head_camera', marker_id, rospy.Time(0))
+                trans.append(trans_new)
+                rot.append(rot_new)
+                dist.append(np.linalg.norm(trans_new))
+                id_list.append(n)
+        if len(dist)>0:     
+            tag_index = np.argmin(dist)
+            print "tag index is", tag_index
+            tag_selected = id_list[tag_index]
+            tag_transform =  (trans[tag_index],rot[tag_index])
+        #could add a statement in here to see if block is too close to neighbors
         
-    #for i in trans,rot:
-        #run until both lists are full? or until some length paramet is satisfied?
-    print trans
-    #call select tag function
-    #select_tag(trans,rot)
-    #subrate.sleep()
-
+    print "Distance is:", dist[np.argmin(dist)]
+    print "Tag Selected is:",tag_selected
+    print "Transform Selected is:", tag_transform
     
-def get_image():
-    #img = Image()
-    rospy.init_node('image_data', anonymous=False)
-    rate = rospy.Rate(20) # 10hz
-    #rostopic pub /robot/head/command_head_pan baxter_core_msgs/HeadPanCommand -- 0 6
-    #time_now =rospy.Time.now()
-    count = 0
-    while not rospy.is_shutdown():
-        #rospy.Subscriber("/usb_cam/image_raw", Image, xdisplay_pub)
-        #rospy.subscriber("/cameras/right_hand_camera/image",Image, get_tag_pose)
-        #if count%10 == 0:
-            #rospy.Subscriber("/cameras/right_hand_camera/image", Image, xdisplay_pub)
-        #rospy.Subscriber("/usb_cam/image_raw", Image, xdisplay_pub)
-        count = count + 1
-        #if count%2 == 0
-        #    rospy.Subscriber("/cameras/right_hand_camera/image", Image, get_tag_pose)
-        rospy.Subscriber("/usb_cam/image_raw", Image, get_tag_pose)
-        rate.sleep()
-
-
+    return tag_selected, tag_transform
+       
+    
 def main_func():
-    get_image()
+    #img = Image()
+    rospy.init_node('tag_select', anonymous=False)
+    rate = rospy.Rate(1) # 10hz
+    first_count = True
+    listener = tf.TransformListener()
+    trans = []
+    rot = []
+    dist = []
+    id_list = []
+    while not rospy.is_shutdown():
+        #if count%10 == 0:
+            #rospy.Subscriber("/usb_cam/image_raw", Image, xdisplay_pub)
+            #rospy.Subscriber("/cameras/right_hand_camera/image", Image, xdisplay_pub)
+        #if count%5 == 0
+        #    rospy.Subscriber("/cameras/right_hand_camera/image", Image, get_tag_pose)
+        '''
+        list_frames = listener.getFrameStrings()
+        marker_found = listener.frameExists('ar_marker_5')
+        if marker_found == True:
+            (trans_new,rot_new) = listener.lookupTransform('head_camera', 'ar_marker_5', rospy.Time(0))
+            #print trans_new
+        
+        if marker_found == True and first_count == True:
+            first_count = False 
+            (trans_old,rot_old) = (trans_new,rot_new)
+            trans.append(trans_new)
+            rot.append(rot_new)
+            print "Marker Found2"
+            print trans
+        
+        if marker_found == True and first_count == False:
+            for i in range(len(trans)):
+            a = np.round(trans_old[0],2).tolist()
+            b = np.round(trans_new[0],2).tolist()
+            print "New Marker??"
+            
+            if a != b:
+                print "NEW MARKER FOUND"
+                (trans_old,rot_old) = (trans_new, rot_new)
+                trans.append(trans_new)
+                rot.append(rot_new)
+        '''
 
-
-
-# this shows you how to subscripe to images in ROS
-#http://wiki.ros.org/rospy_tutorials/Tutorials/WritingImagePublisherSubscriber
-
-#open the head camera using the 
-#rosrun baxter_tools camera_control.py --open head_camera --resolution 1280x800
-#look at the baxter python API to get connection to camera
-#http://sdk.rethinkrobotics.com/wiki/Hello_Baxter#Step_4:_Run_an_Example_Program
-
- 
-# he messages forma
-#http://docs.ros.org/api/sensor_msgs/html/msg/CompressedImage.html
-#rospy.Subscriber('/camerasleft_hand_camera/image', CompressedImage, ?,?)
-#note that classes support two kinds of operations, attribute references and instantiation
-#Alos classes can be assigned to variables
-#how? if JointStates is a class (coming from sensor messages)then js = JointStates() assings this in the form 
-#of a function
-#js.header.stamp = rospy.Time.now() assigns the time given by rospy to the stamp attribute
-# of js.
-
+        '''
+        try:
+           (trans_new,rot_new) = listener.lookupTransform('head_camera', 'ar_marker_5', rospy.Time(0))
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+           marker_found = False
+           #print "Marker Found?:", marker_found
+        else: 
+           #print trans
+           x =1
+        '''
+        #if we want to use multiple markers and pick them up based on distance
+        tag_selected, tag_transform = multi_marker(trans,rot,dist,id_list, listener)
+        rate.sleep()
 
 if __name__ == '__main__':
     
